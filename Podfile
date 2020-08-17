334eda02130634af9b87945dbe3c69453c1d3ada
rider <1308340537@qq.com>
2020-08-17 14:36:37 +0800

    Update - init pod

---
+++ Podfile
@@ -0,0 +1,13 @@
+
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+inhibit_all_warnings!
+use_frameworks!
+
+target 'YiBaseProductSwift' do
+
+  pod 'PromiseKit', '6.10.0'
+  pod 'Moya/RxSwift', '14.0.0-alpha.2'
+  pod 'RxCocoa', '5.0.0'
+
+end
---
+++ YiBaseProductSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YiBaseProductSwift
+//
+//  Created by 任斌 on 2020/8/13.
+//  Copyright © 2020 任斌. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
