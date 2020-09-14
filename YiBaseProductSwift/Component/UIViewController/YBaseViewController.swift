632da8244670f20ea107e38152ab1dc82d3f4831
zero_rb <1308340537@qq.com>
2020-09-14 16:12:18 +0800

    Update - record navigationBar.shadowImage

--- YiBaseProductSwift/Component/UIViewController/YBaseViewController.swift
+++ YiBaseProductSwift/Component/UIViewController/YBaseViewController.swift
@@ -9,11 +9,25 @@
 import UIKit
 
 class YBaseViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    //MARK: public property
+    public var isHiddenNavigationLine: Bool = false {
+        willSet {
+            if newValue {
+                _navigationLineImage = navigationController?.navigationBar.shadowImage
+                navigationController?.navigationBar.shadowImage = UIImage()
+            } else {
+                navigationController?.navigationBar.shadowImage = _navigationLineImage
+            }
+        }
     }
     
+    //MARK: private property
+    private var _navigationLineImage: UIImage?
+}
+
+//MARK: screen ratate
+extension YBaseViewController {
     override var shouldAutorotate: Bool {
         return false
     }
@@ -22,3 +36,5 @@ class YBaseViewController: UIViewController {
         return .portrait
     }
 }
+
+
